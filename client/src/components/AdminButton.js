190517dee1d28cc1c3d4ca2767fd46cb6341c729
DCebo <d.ceballos575@gmail.com>
2021-02-17 02:36:06 -0500

    Admin button to move across client + server style

--- client/src/components/AdminButton.js
+++ client/src/components/AdminButton.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../css/AdminButton.css'
 
 function AdminButton() {
+
     return (
-    <button className="admin_btn"onClick={(e) => (window.location = 'http://localhost:4000/v1')}>
-        Admin
-    </button>
+        <button className="admin_btn" onClick={(e) => (window.location = 'http://localhost:4000/v1')}>
+            Admin
+        </button>
+
     )
 }
 
